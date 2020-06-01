8c4ca439f570bdf9786dea8be76dd2da689433f9
SSong-develop <thdgnsrl123@naver.com>
2020-06-02 03:17:02 +0900

    // All Function is done // splash done

---
+++ app/src/main/java/com/example/mylittleobserver_android/Activities/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.mylittleobserver_android.Activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+
+import com.example.mylittleobserver_android.R;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Handler hd = new Handler();
+        hd.postDelayed(new splashhandler(),3000);
+    }
+
+    private class splashhandler implements Runnable{
+        public void run(){
+            startActivity(new Intent(SplashActivity.this,AddDeviceActivity.class));
+            finish();
+        }
+
+    }
+}
